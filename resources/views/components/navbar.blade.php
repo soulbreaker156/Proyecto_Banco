8988981d3e3951dc663313752adbb18bcd8dfad1
Jostyn <soulbreaker0220@gmail.com>
2025-05-09 09:52:20 -0600

    Add: footer de la pagina

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,4 +1,4 @@
-<div class=" flex flex-col w-[15%] bg-gray-800 text-white p-4 gap-3" >
+<div class=" flex h-screen flex-col w-[15%] bg-gray-800 text-white p-4 gap-3" >
    <div class="flex justify-center bg-slate-50 p-4 my-1 text-2xl rounded-md">
       <h1 class="text-black">Banco</h1>
    </div>
