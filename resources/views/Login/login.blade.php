3498fe6826b585cf3e84b941cd9034db5b510bb9
Jostyn <soulbreaker0220@gmail.com>
2025-06-13 21:39:54 -0600

    Agregado otras cosas

---
+++ resources/views/Login/login.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    <link href="{{ mix('js/app.js') }}" rel="stylesheet">
+    <title>Login</title>
+</head>
+<body class="flex bg-[#dbcbdf]">
+    
+</body>
+</html>
